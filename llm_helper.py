b70a560dc477824664ffc9f71e9b78627867ea09
TUSHAR KANTI PATRA <141545071+Tushar-CYL@users.noreply.github.com>
2024-11-11 08:53:17 +0530

    Update llm_helper.py

--- llm_helper.py
+++ llm_helper.py
@@ -1,9 +1,12 @@
 from langchain_groq import ChatGroq
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
-llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.2-90b-text-preview")
+# load_dotenv()
+# llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.2-90b-text-preview")
+# Directly specifying the API key in the code without using environment variables
+llm = ChatGroq(groq_api_key="YOUR_API_KEY_HERE", model_name="llama-3.2-90b-text-preview")
+
 
 
 if __name__ == "__main__":
